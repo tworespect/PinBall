fc607ae166186e2b8e202f34f2a3713724ff6c73
紀　祐司 <signtheswish@gmail.com>
2016-12-08 08:22:25 +0900

    PinBallプロジェクトの初めてのコミット
    対象物に衝突したら点数を加算するファイルです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	//点数を表示するテキスト
+	private GameObject pointText;
+
+	//初期値は0
+	private int point = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+
+		//シーン中のPonitTextオブジェクトを取得
+		this.pointText = GameObject.Find("PointText");
+
+		//初期は0
+		this.pointText.GetComponent<Text>().text = ((int)point).ToString();
+
+
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバßを表示
+			this.gameoverText.GetComponent<Text> ().text = "GameOver"; 
+		}
+
+	}
+			
+	void OnCollisionEnter(Collision other){
+			
+		if (other.gameObject.tag == "SmallStarTag") {
+
+			this.point -= 100;
+
+			this.pointText.GetComponent<Text>().text = ((int)point).ToString();
+		}
+
+		if (other.gameObject.tag == "LargeStarTag") {
+
+			this.point -= 10;
+
+			this.pointText.GetComponent<Text>().text = ((int)point).ToString();
+		}
+
+		if (other.gameObject.tag == "SmallCloudTag") {
+
+			this.point += 200;
+
+			this.pointText.GetComponent<Text>().text = ((int)point).ToString();
+		}
+
+		if (other.gameObject.tag == "LargeCloudTag") {
+
+			this.point += 34;
+
+			this.pointText.GetComponent<Text>().text = ((int)point).ToString();
+		}
+
+
+	}
+
+
+}
---
+++ Assets/StarController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour {
+
+
+	//回転速度を指定する
+	private float rotSpeed = 1.0f;
+
+
+	//Start関数内に立ち上げ時の状態を設定
+	void Start() {
+
+		//回転する開始角度を指定する　＊複数回転させたい対象物がある
+		this.transform.Rotate(0 , Random.Range(0,360) , 0);
+
+	}
+
+	//回転対象物の状態を設定
+
+	void Update(){
+
+		this.transform.Rotate (0, this.rotSpeed, 0);
+	
+
+
+
+	}
+
+
+
+
+
+}
